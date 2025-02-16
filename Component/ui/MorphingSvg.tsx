4ff9b1c7c90d640db74f2c3b5e84eacb73ef4412
DylanTigo <dylannoeltigomo@gmail.com>
2025-02-16 22:14:49 +0100

    Component with the effect needed done

---
+++ Component/ui/MorphingSvg.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import * as d3 from "d3";
+import Image from "next/image";
+import { useEffect, useRef } from "react";
+
+const MorphingSVG = () => {
+  const ref = useRef(null);
+  const width = 450,
+    height = 450;
+
+  useEffect(() => {
+    const svg = d3.select(ref.current);
+
+    // Définition de l'arc
+    const arc = d3
+      .arc()
+      .innerRadius(0) // Cercle plein
+      .outerRadius(width / 2) // Rayon max du masque
+      .startAngle(0)
+      .endAngle(0); // Début fermé
+
+    // Ajout du clipPath
+    const clipPath = svg
+      .append("clipPath")
+      .attr("id", "circle-mask")
+      .append("path")
+      .attr("d", arc)
+      .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+    // Animation d'ouverture
+    clipPath
+      .transition()
+      .duration(2000)
+      .attrTween("d", function () {
+        const interpolate = d3.interpolate(0, 2 * Math.PI);
+        return function (t: number) {
+          return arc.startAngle(interpolate(t))();
+        };
+      });
+  }, []);
+
+  return (
+    <>
+      <svg ref={ref} width={width} height={height} transform="rotate(-135)">
+        {/* Image masquée par le clipPath */}
+        <image
+          href="/image.png"
+          style={{ objectFit: "cover", transformOrigin: "center", rotate: "135deg", objectPosition: "center" }}
+          width={width}
+          clipPath="url(#circle-mask)"
+        />
+      </svg>
+    </>
+  );
+};
+
+export default MorphingSVG;
